f1775769453065ccdc8c716575a8cb188a4fe977
0v0d <energyremtu@gmail.com>
2024-05-06 21:21:25 +0900

    ResultFragmentの追加

---
+++ app/src/main/java/com/example/android_githubactions_sample/view/ResultFragment.kt
@@ -0,0 +1,45 @@
+package com.example.android_githubactions_sample.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.navArgs
+import com.example.android_githubactions_sample.databinding.FragmentResultBinding
+import com.example.android_githubactions_sample.viewmodel.ResultViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class ResultFragment : Fragment() {
+    private var _binding: FragmentResultBinding? = null
+    private val binding get() = _binding!!
+    private val args: ResultFragmentArgs by navArgs()
+    private val viewModel: ResultViewModel by viewModels()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentResultBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.searchLocation(args.inputText)
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.result.collect {
+                binding.locationData = it
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
