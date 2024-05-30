a1988039bb4e04597e6adec8063aa56e50442e3d
0v0d <energyremtu@gmail.com>
2024-05-30 20:02:51 +0900

    test

--- app/src/main/java/com/example/androidgithubactionssample/MainActivity.kt
+++ app/src/main/java/com/example/androidgithubactionssample/MainActivity.kt
@@ -8,13 +8,8 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState:
-
-                          Bundle?) {
-        super.onCreate(savedInstanceState
-
-
-        )
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
