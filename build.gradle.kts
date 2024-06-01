6a107216de186bddb56d95ff508d0af861981130
0v0d <energyremtu@gmail.com>
2024-06-01 17:53:31 +0900

    hiltの修正

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,13 @@ plugins {
     alias(libs.plugins.dagger.hilt.android) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.secrets.gradle.plugin) apply false
+    alias(libs.plugins.ktlint) apply false
+}
+allprojects {
+    afterEvaluate {
+        tasks.withType<JavaCompile>().configureEach {
+            // 警告メッセージの設定
+            options.compilerArgs.addAll(listOf("-Xlint:unchecked", "-Xlint:deprecation"))
+        }
+    }
 }
